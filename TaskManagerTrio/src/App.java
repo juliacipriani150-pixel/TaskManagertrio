a926ab5892636edd2202f3e019431dd31f84f589
Juh Cipriani <julia_cipriani150@estudante.sesisenai.org.br>
2025-09-01 16:49:14 -0300

    Update App.java

--- TaskManagerTrio/src/App.java
+++ TaskManagerTrio/src/App.java
@@ -1,5 +1,15 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.ArrayList;
+import java.util.List;
+
+public class GerenciadorTarefas {
+    private final List<Tarefa> tarefas = new ArrayList<>();
+
+     // Dev C
+    public boolean removerTarefa(int indice) {
+        if (indice >= 0 && indice < tarefas.size()) {
+            tarefas.remove(indice);
+            return true;
+        }
+        return false;
     }
 }
