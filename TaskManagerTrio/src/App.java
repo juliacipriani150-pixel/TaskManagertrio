68b52dc5bc7106584ee9a4368f28c7815070b864
Juh Cipriani <julia_cipriani150@estudante.sesisenai.org.br>
2025-09-01 16:51:13 -0300

    Update App.java

--- TaskManagerTrio/src/App.java
+++ TaskManagerTrio/src/App.java
@@ -1,15 +1,1 @@
-import java.util.ArrayList;
-import java.util.List;
 
-public class GerenciadorTarefas {
-    private final List<Tarefa> tarefas = new ArrayList<>();
-
-     // Dev C
-    public boolean removerTarefa(int indice) {
-        if (indice >= 0 && indice < tarefas.size()) {
-            tarefas.remove(indice);
-            return true;
-        }
-        return false;
-    }
-}
